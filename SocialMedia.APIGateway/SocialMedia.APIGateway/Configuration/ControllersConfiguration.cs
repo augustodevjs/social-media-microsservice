93f894bf828afeae7d2cfe2ed9d033e10240a9a2
augusto <augustosaaude@gmail.com>
2024-08-19 22:15:44 -0300

    feat: add ocelot with swagger

---
+++ SocialMedia.APIGateway/SocialMedia.APIGateway/Configuration/ControllersConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Ocelot.DependencyInjection;
+
+namespace SocialMedia.APIGateway.Configuration;
+
+public static class ControllersConfiguration
+{
+    public static IServiceCollection AddAndConfigureControllers(this IServiceCollection services)
+    {
+        services
+            .AddDocumentation()
+            .AddApiGateway();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDocumentation(this IServiceCollection services)
+    {
+        services
+            .AddSwaggerGen()
+            .AddEndpointsApiExplorer();
+
+        return services;
+    }
+
+    private static IServiceCollection AddApiGateway(this IServiceCollection services)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("ocelot.json")
+            .Build();
+
+        services.AddOcelot(configuration);
+        services.AddSwaggerForOcelot(configuration);
+
+        return services;
+    }
+
+    public static WebApplication UseDocumentation(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
+        }
+
+        return app;
+    }
+}
--- SocialMedia.APIGateway/SocialMedia.APIGateway/Program.cs
+++ SocialMedia.APIGateway/SocialMedia.APIGateway/Program.cs
@@ -1,33 +1,21 @@
+using Ocelot.Middleware;
+using SocialMedia.APIGateway.Configuration;
 
 namespace SocialMedia.APIGateway;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddAndConfigureControllers();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
+        app.UseDocumentation();
+        await app.UseOcelot();
         app.UseAuthorization();
-
-
         app.MapControllers();
 
         app.Run();
